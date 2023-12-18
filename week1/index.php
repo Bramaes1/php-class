976231e74606fa9be20b38468a0426cf461e18f3
Bramaes Anantanatorn <154107726+Bramaes1@users.noreply.github.com>
2023-12-18 11:07:23 +0700

    เพิ่มไฟล์ week1

---
+++ week1/index.php
@@ -0,0 +1,9 @@
+<?php
+    $address = "moo.3 wattananuwong";
+    $char1 = 'A';
+    //character => A,B,C,D
+    //sny chaacer => CAT, D
+    echo $address[0].$address[1].$address[2].$address[3].$address[4];
+    echo $char1;
+
+?>
